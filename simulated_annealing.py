bd75e9c8fb4e68907e22a014c768e1e881d92113
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-04-17 18:47:50 +0200

    tested implemented for simulated annealing

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 import math
+import time
+import ZhangOptimization as zo
 
 # Call it to generate the first point
 def random_start(low, upper, num):
@@ -29,7 +31,7 @@ def acceptance_probability(value, new_value, temperature):
         # we accept non-improvement value with a certain probability
         return p
 
-def simulated_annealing(start, loss_function, m, w, random_neighbour, low, upper, acceptance, temperature, max_iter=1000,):
+def simulated_annealing(start, loss_function, m, w, low, upper, max_iter=1000,):
     "start is a np array, random_neighbour, acceptance and temperature are functions"
     # number of dimensions
     dim = start.shape[0]
@@ -39,8 +41,37 @@ def simulated_annealing(start, loss_function, m, w, random_neighbour, low, upper
     for step in range(max_iter):
         fraction = step / float(max_iter)
         t = temperature(fraction)
-        neighbour = random_neighbour(point, fraction)
+        neighbour = random_neighbour(point, low, upper, fraction)
         neighbour_value = loss_function(m, np.reshape(neighbour,(3,3)), w)
         if acceptance_probability(value, neighbour_value, t) > np.random.rand():
             point, value = neighbour, neighbour_value
     return (value, point)
+
+def main():
+    start = time.time()
+    # Number of dimension
+    DIM = 9
+    # low and upper bounds
+    LOW = 0
+    UPPER = 1000
+    # Set the name of the image file
+    img = 'Chessboard.jpg'
+    # Get m and w that represent respectively the image coordinates and the world coordinates already trasformed from R to P
+    # It takes about 1 minute
+    m , w = zo.process_corners(img)
+    # Zhang optimization step (minimization of the distance from real coordinates in image plan and the ones found by the corner detector)
+    # generating starting points
+    np.random.seed(50)
+    starting_point = random_start(LOW, UPPER, DIM)
+    #for point in starting_points:
+    #    print(point)
+    # best_homography is a tuple
+    best_homography = simulated_annealing(starting_point, zo.loss_function, m, w, LOW, UPPER)
+    #Function that prints the points of the image and the projection error refering to the optimal H
+    m = m[:,:2]
+    w = np.reshape(best_homography[1],(3,3)) @ w.T
+    w = w.T[:,:2]
+    zo.print_correspondences(img,best_homography[1],best_homography[0],m,w)
+    print("Time: {}".format(time.time() - start))
+
+main()
