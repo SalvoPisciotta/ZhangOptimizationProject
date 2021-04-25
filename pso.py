7e35ae7c4f15fa8f2ff09df2b65dafa955a4ae31
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-04-25 18:43:06 +0200

    Introduction of lbest, max velocity for avoiding a divergence speed, constriction formula (not original one), attempt using different parameters wrt original model

--- pso.py
+++ pso.py
@@ -1,34 +1,71 @@
 import numpy as np
 import time
 import ZhangOptimization as zo
+        
+def particle_swarm_optimization(loss, m, w, bounds, c1, c2, num_par, vmax, tol = 1e-13, max_iter = 10000):
 
-# get the initial best position for each particle
-'''
-def get_particle_best(particle_pos,particle_val, num_par):
-    # number of near position to consider
-    NUM_NEIGHBOURS = 3
-    particle_best = np.empty(num_par)
-    # finding the best position for each particle
-    for j in range(num_par):
-        local_vals = np.zeros(NUM_NEIGHBOURS)
-        local_vals[0] = particle_pos_val[j-2]
-        local_vals[1] = particle_pos_val[j-1]
-        local_vals[2] = particle_pos_val[j]
-        min_index=int(np.argmin(local_vals))
-        local_best[j-1]=particle_pos[min_index+j-2][:]
-    return np.array(local_best)
+    # getting initial particles and other related data
+    dim, particle_pos, particle_val, particle_velocity, particle_best, swarm_best, local_best = random_inizialization(loss, m, w, bounds, num_par)
+    # last best swarm position
+    old_swarm = np.zeros(dim)
+    # implementing constriction factor
+    phi = c1 + c2
+    chi = 2/(abs(2-phi-np.sqrt((phi**2)-(4*phi))))
+    # number of iterations
+    iter = 0
 
-    return 0
-'''
+    while abs(loss(m, np.reshape(old_swarm, (3,3)), w) - loss(m, np.reshape(swarm_best, (3,3)), w)) > tol and iter < max_iter:
 
+        iter += 1
+        print("Step {}".format(iter))
+        time.sleep(0.300)
 
+        for i in range(num_par):
+            eps_1, eps_2 = np.random.uniform(0,1,2)
+            particle_velocity[i,:] += (c1*eps_1*(particle_best[i,:]-particle_pos[i,:]))
+            particle_velocity[i,:] += (c2*eps_2*(local_best[i,:]-particle_pos[i,:]))
+            particle_velocity[i,:] = particle_velocity[i,:]*chi
+            # velocity is too high
+            if particle_velocity[i].any() > vmax :
+                particle_velocity[i,:] = vmax 
+            # updating the position
+            particle_pos[i,:] += particle_velocity[i,:]
+
+            particle_fitness = loss(m, np.reshape(particle_pos[i,:], (3,3)), w)
+        
+            # model implementation
+            if particle_fitness < particle_val[i]:
+                particle_best[i,:] = particle_pos[i,:]
+                partcle_val[i] = particle_fitness
+                f_swarm_best = loss(m, np.reshape(swarm_best, (3,3)), w)
+                if particle_fitness < f_swarm_best: 
+                    old_swarm_best = swarm_best[:]
+                    swarm_best = particle_best[i,:].copy()
+            
+            local_best = get_local_best(particle_pos, particle_val, num_par)
+
+            # my implementation
+            '''particle_val[i] = loss(m, np.reshape(particle_pos[i,:], (3,3)), w)
+            # the value of the new position is better than the current best value for this particle
+            if particle_val[i] < loss(m, np.reshape(particle_best[i], (3,3)), w):
+                particle_best[i,:] = particle_pos[i,:]
+                if particle_val[i] < loss(m, np.reshape(swarm_best, (3,3)), w):
+                    old_swarm = swarm_best
+                    swarm_best = particle_pos[i,:]'''
+
+    return (loss(m, np.reshape(swarm_best, (3,3)), w), swarm_best)
+
+# random initializion of the swarm
 def random_inizialization(f, m, w, bounds, num_par):
-
+    
     # getting number of dimension
     dim = len(bounds)
+    # position of the particles
     particle_pos = np.zeros(num_par)
     particle_pos = particle_pos.tolist()
+    # velocity of the particles
     particle_velocity = particle_pos[:]
+    # current value of the particle
     particle_val = particle_pos[:]
 
     for j in range(num_par):
@@ -38,43 +75,29 @@ def random_inizialization(f, m, w, bounds, num_par):
         particle_val[j] = f(m, np.reshape(particle_pos[j], (3,3)), w)
         # velocity for each particle
         particle_velocity[j]=[np.random.uniform(-abs(bounds[i][1]-bounds[i][0]) ,abs(bounds[i][1]-bounds[i][0])) for i in range(dim)]
-    # best position for each particle
-    particle_best = particle_pos.copy()
-    # best global position
+    
+    # getting local best position using lbest ring topology
+    local_best = get_local_best(particle_pos, particle_val, num_par)
+    # getting swarm best position
     swarm_best = particle_pos[np.argmin(particle_val)]
+    #setting all particles current positions to best
+    particle_best = particle_pos.copy()
 
-    return dim, np.array(particle_pos), np.array(particle_val), np.array(particle_velocity), np.array(particle_best), np.array(swarm_best)
-        
-def particle_swarm_optimization(loss, m, w, bounds, omega, phi_p, phi_g, num_par, tol = 1e-10):
+    return dim, np.array(particle_pos), np.array(particle_val), np.array(particle_velocity), np.array(particle_best), np.array(swarm_best), np.array(local_best)
 
-    # getting initial particles and other related data
-    dim, particle_pos, particle_val, particle_velocity, particle_best, swarm_best = random_inizialization(loss, m, w, bounds, num_par)
-
-    # last best global point
-    old_swarm = np.zeros(dim)
-
-    while abs(loss(m, np.reshape(old_swarm, (3,3)), w) - loss(m, np.reshape(swarm_best, (3,3)), w)) > tol:
-        for i in range(num_par):
-            r_p, r_g = np.random.uniform(0,1,2)
-            particle_velocity[i,:] += (phi_p * r_p * (particle_best[i,:] - particle_pos[i,:]))
-            particle_velocity[i,:] += (phi_g * r_g * (swarm_best[i,:] - particle_pos[i,:]))
-            particle_velocity[i,:] = particle_velocity[i,:] * omega
-            #if particle_velocity[i].any() > vmax : #is any velocity is greater than vmax
-                    #particle_velocity[i,:]=vmax #set velocity to vmax
-            #all of the above is regarding updating the particle's velocity
-            #with regards to various parameters (local_best, p_best etc..)
-            # updating position
-            particle_pos[i,:] += particle_velocity[i,:]
-
-            particle_val[i] = loss(m, np.reshape(particle_pos[i,:], (3,3)), w)
-            # the value of the new position is better than the current best value for this particle
-            if particle_val[i] < loss(m, np.reshape(particle_best[i], (3,3)), w):
-                particle_best[i,:] = particle_pos[i,:]
-                if particle_val[i] < loss(m, np.reshape(swarm_best, (3,3)), w):
-                    old_swarm = swarm_best
-                    swarm_best = particle_pos[i,:]
-
-    return (loss(m, np.reshape(swarm_best, (3,3)), w), swarm_best)
+# implementing local best topology
+def get_local_best(particle_pos, particle_val, num_par):
+    # creation of an empty list
+    local_best = [0] * num_par
+    # for each particle finding the best position looking at its neighbours (ring topology)
+    for j in range(num_par):
+        local_values = np.zeros(3)
+        local_values[0] = particle_val[j-2]
+        local_values[1] = particle_val[j-1]
+        local_values[2] = particle_val[j]
+        min_index = int(np.argmin(local_values))
+        local_best[j-1] = particle_pos[min_index+j-2][:]
+    return np.array(local_best)
 
 if __name__ == '__main__':
     start = time.time()
@@ -89,15 +112,17 @@ if __name__ == '__main__':
     w = w[:8,:]
     # Zhang optimization step (minimization of the distance from real coordinates in image plan and the ones found by the corner detector)
     # generating bounds for initial point
+    dimension_bounds = [0,1000]
     bounds = []
     for i in range(DIM):
-        bounds.append([0,100])
+        bounds.append(dimension_bounds)
+    # computing vmax
+    vmax = (dimension_bounds[1]-dimension_bounds[0])*0.75
     # parameters have been get consulting a scientific paper
-    omega = -0.3488
-    phi_p = -0.2746
-    phi_g = 4.8976
+    c1 = -0.2746
+    c2 = 4.8976
     num_par = 53
-    best_homography = particle_swarm_optimization(zo.loss_function, m, w, bounds, omega, phi_p, phi_g, num_par)
+    best_homography = particle_swarm_optimization(zo.loss_function, m, w, bounds, c1, c2, num_par, vmax)
     #Function that prints the points of the image and the projection error refering to the optimal H
     m = m[:,:2]
     w = np.reshape(best_homography[1],(3,3)) @ w.T
